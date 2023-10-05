06fe9f21adcfb1585aba8b462cae5e5345b34c90
Rogerio Battistoni Filho <robattistoni@gmail.com>
2023-10-05 20:55:37 -0300

    Upload

---
+++ src/svg/hamburguerMenu.tsx
@@ -0,0 +1,8 @@
+export default function hamburguerMenu() {
+    return (
+<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4 7H20M4 12H20M4 17H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+    )
+  }
+  
